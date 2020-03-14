9a00316f4de21d6da65f8ca7499182d214dfa102
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2020-03-14 20:17:39 +0900

    version bump

--- SanyaPlugin/SanyaPlugin.cs
+++ SanyaPlugin/SanyaPlugin.cs
@@ -10,7 +10,7 @@ namespace SanyaPlugin
     {
         public override string getName { get; } = "SanyaPlugin";
         public static readonly string harmonyId = "jp.sanyae2439.SanyaPlugin";
-        public static readonly string Version = "1.2.7d";
+        public static readonly string Version = "1.2.7e";
         public static readonly string TargetVersion = "1.8.12";
         public static readonly string PlayersDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Plugins", "SanyaPlugin");
 
