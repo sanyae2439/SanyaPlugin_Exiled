3230334bf2d950477d61b556d08f0ae460fbc598
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2020-07-11 10:34:52 +0900

    2.0.4c

--- SanyaPlugin/Functions.cs
+++ SanyaPlugin/Functions.cs
@@ -610,6 +610,59 @@ namespace SanyaPlugin.Functions
 			if(CustomLiteNetLib4MirrorTransport.DelayConnections) return -1;
 			return Cassie.mtfRespawn.ChaosRespawnTickets;
 		}
+
+		public static void SendCustomSyncVar(this ReferenceHub player, NetworkIdentity behaviorOwner, Type targetType, Action<NetworkWriter> customSyncVar)
+		{
+			/* 
+			Example:
+			player.SendCustomSyncVar(player.networkIdentity, typeof(ServerRoles), (targetwriter) =>
+			{
+				targetwriter.WritePackedUInt64(2UL);
+				targetwriter.WriteString("test");
+			});
+			 */
+			NetworkWriter writer = NetworkWriterPool.GetWriter();
+			NetworkWriter writer2 = NetworkWriterPool.GetWriter();
+			MakeCustomSyncVarWriter(behaviorOwner, targetType, customSyncVar, writer, writer2);
+			NetworkServer.SendToClientOfPlayer(player.networkIdentity, new UpdateVarsMessage() { netId = behaviorOwner.netId, payload = writer.ToArraySegment() });
+			NetworkWriterPool.Recycle(writer);
+			NetworkWriterPool.Recycle(writer2);
+		}
+
+		public static void MakeCustomSyncVarWriter(NetworkIdentity behaviorOwner, Type targetType, Action<NetworkWriter> customSyncVar, NetworkWriter owner, NetworkWriter observer)
+		{
+			ulong dirty = 0ul;
+			ulong dirty_o = 0ul;
+			NetworkBehaviour behaviour = null;
+			for(int i = 0; i < behaviorOwner.NetworkBehaviours.Length; i++)
+			{
+				behaviour = behaviorOwner.NetworkBehaviours[i];
+				if(behaviour.GetType() == targetType)
+				{
+					dirty |= 1UL << i;
+					if(behaviour.syncMode == SyncMode.Observers) dirty_o |= 1UL << i;
+				}
+			}
+			owner.WritePackedUInt64(dirty);
+			observer.WritePackedUInt64(dirty & dirty_o);
+
+			int position = owner.Position;
+			owner.WriteInt32(0);
+			int position2 = owner.Position;
+
+			behaviour.SerializeObjectsDelta(owner);
+			customSyncVar(owner);
+			int position3 = owner.Position;
+			owner.Position = position;
+			owner.WriteInt32(position3 - position2);
+			owner.Position = position3;
+
+			if(dirty_o != 0ul)
+			{
+				ArraySegment<byte> arraySegment = owner.ToArraySegment();
+				observer.WriteBytes(arraySegment.Array, position, owner.Position - position);
+			}
+		}
 	}
 
 	internal static class Extensions
