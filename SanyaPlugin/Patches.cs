a70a8a0880969465b09a6b6d6249da84e8d519e9
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2020-09-27 15:11:05 +0900

    2.5.3

--- SanyaPlugin/Patches.cs
+++ SanyaPlugin/Patches.cs
@@ -726,4 +726,14 @@ namespace SanyaPlugin.Patches
 			}
 		}
 	}
+
+	//not override
+	[HarmonyPatch(typeof(SinkholeEnvironmentalHazard), nameof(SinkholeEnvironmentalHazard.OnStart))]
+	public static class SinkHoleSCPEnablePatch
+	{
+		public static void Prefix(SinkholeEnvironmentalHazard __instance)
+		{
+			__instance.SCPImmune = false;
+		}
+	}
 }
