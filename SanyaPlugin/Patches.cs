052662ba3f170fc30ec021429ce809d4dfcabbed
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2020-10-19 00:01:11 +0900

    2.7.2

--- SanyaPlugin/Patches.cs
+++ SanyaPlugin/Patches.cs
@@ -121,6 +121,27 @@ namespace SanyaPlugin.Patches
 		}
 	}
 
+	//override - Fixed
+	[HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.RemovePlayer))]
+	public static class IdleModePatch
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var newInst = instructions.ToList();
+			var index = newInst.FindLastIndex(x => x.opcode == OpCodes.Brtrue_S) + 1;
+
+			newInst.InsertRange(index, new[] 
+			{ 
+				new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(RoundSummary), nameof(RoundSummary.singleton))),
+				new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(RoundSummary), nameof(RoundSummary._roundEnded))),
+				new CodeInstruction(OpCodes.Brtrue_S, newInst[newInst.Count - 1].labels[0])
+			});
+
+			for(int i = 0; i < newInst.Count; i++)
+				yield return newInst[i];
+		}
+	}
+
 	//override - 10.0.0 checked
 	[HarmonyPatch(typeof(Intercom), nameof(Intercom.UpdateText))]
 	public static class IntercomTextPatch
