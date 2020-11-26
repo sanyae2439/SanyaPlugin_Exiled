d2cdcde17165f4ee9d603bf8ad680b0b6773e214
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2020-11-26 13:50:03 +0900

    2.13.2

--- SanyaPlugin/SanyaPlugin.cs
+++ SanyaPlugin/SanyaPlugin.cs
@@ -24,7 +24,7 @@ namespace SanyaPlugin
 		public override string Author => "sanyae2439";
 		public override PluginPriority Priority => PluginPriority.Default;
 		public override Version Version => new Version(Assembly.GetName().Version.Major, Assembly.GetName().Version.Minor, Assembly.GetName().Version.Build);
-		public override Version RequiredExiledVersion => new Version(2, 1, 15);
+		public override Version RequiredExiledVersion => new Version(2, 1, 18);
 
 		public static SanyaPlugin Instance { get; private set; }
 		public EventHandlers Handlers { get; private set; }
