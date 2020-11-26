a877aa4117f1867d04555db4aed8e270f5bd0ba3
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2020-11-26 17:41:39 +0900

    update CustomSyncVar

--- SanyaPlugin/Functions.cs
+++ SanyaPlugin/Functions.cs
@@ -21,7 +21,6 @@ using Exiled.Events.EventArgs;
 using Exiled.API.Features;
 using SanyaPlugin.Data;
 
-
 namespace SanyaPlugin.Functions
 {
 	internal static class PlayerDataManager
@@ -640,6 +639,78 @@ namespace SanyaPlugin.Functions
 			NetworkServer.SendToClientOfPlayer(player.ReferenceHub.networkIdentity, new UpdateVarsMessage() { netId = behaviorOwner.netId, payload = writer.ToArraySegment() });
 			NetworkWriterPool.Recycle(writer);
 			NetworkWriterPool.Recycle(writer2);
+		}
+
+		public static void SendCustomSyncVar(this Player player, NetworkIdentity behaviorOwner, Type targetType, string PropertyName, object value)
+		{			
+			// Example [TargetOnlyBadge]:
+			// player.SendCustomSyncVar(player.ReferenceHub.serverRoles.netIdentity, typeof(ServerRoles), nameof(ServerRoles.NetworkMyText), "test");
+
+			Action<NetworkWriter> customSyncVarGenerator = (targetWriter) => {
+				targetWriter.WritePackedUInt64(GetDirtyBit(targetType, PropertyName));
+				GetWriteExtension(value)?.Invoke(null, new object[] { targetWriter, value });
+			};
+
+			NetworkWriter writer = NetworkWriterPool.GetWriter();
+			NetworkWriter writer2 = NetworkWriterPool.GetWriter();
+			MakeCustomSyncWriter(behaviorOwner, targetType, null, customSyncVarGenerator, writer, writer2);
+			NetworkServer.SendToClientOfPlayer(player.ReferenceHub.networkIdentity, new UpdateVarsMessage() { netId = behaviorOwner.netId, payload = writer.ToArraySegment() });
+			NetworkWriterPool.Recycle(writer);
+			NetworkWriterPool.Recycle(writer2);
+		}
+
+		public static ulong GetDirtyBit(Type targetType, string PropertyName)
+		{
+			var bytecodes = targetType.GetProperty(PropertyName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance)?.GetSetMethod().GetMethodBody().GetILAsByteArray();
+			return bytecodes[Array.FindLastIndex(bytecodes, x => x == System.Reflection.Emit.OpCodes.Ldc_I8.Value) + 1];
+		}
+
+		public static System.Reflection.MethodInfo GetWriteExtension(object value)
+		{
+			Type type = value.GetType();
+			switch(Type.GetTypeCode(type))
+			{
+				case TypeCode.String:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteString));
+				case TypeCode.Boolean:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteBoolean));
+				case TypeCode.Int16:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteInt16));
+				case TypeCode.Int32:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WritePackedInt32));
+				case TypeCode.UInt16:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteUInt16));
+				case TypeCode.Byte:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteByte));
+				case TypeCode.SByte:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteSByte));
+				case TypeCode.Single:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteSingle));
+				case TypeCode.Double:
+					return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteDouble));
+				default:
+					if(type == typeof(Vector3))
+						return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteVector3));
+					if(type == typeof(Vector2))
+						return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteVector2));
+					if(type == typeof(GameObject))
+						return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteGameObject));
+					if(type == typeof(Quaternion))
+						return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WriteQuaternion));
+					if(type == typeof(BreakableWindow.BreakableWindowStatus))
+						return typeof(BreakableWindowStatusSerializer).GetMethod(nameof(BreakableWindowStatusSerializer.WriteBreakableWindowStatus));
+					if(type == typeof(Grenades.RigidbodyVelocityPair))
+						return typeof(Grenades.RigidbodyVelocityPairSerializer).GetMethod(nameof(Grenades.RigidbodyVelocityPairSerializer.WriteRigidbodyVelocityPair));
+					if(type == typeof(ItemType))
+						return typeof(NetworkWriterExtensions).GetMethod(nameof(NetworkWriterExtensions.WritePackedInt32));
+					if(type == typeof(PlayerMovementSync.RotationVector))
+						return typeof(RotationVectorSerializer).GetMethod(nameof(RotationVectorSerializer.WriteRotationVector));
+					if(type == typeof(Pickup.WeaponModifiers))
+						return typeof(WeaponModifiersSerializer).GetMethod(nameof(WeaponModifiersSerializer.WriteWeaponModifiers));
+					if(type == typeof(Offset))
+						return typeof(OffsetSerializer).GetMethod(nameof(OffsetSerializer.WriteOffset));
+					return null;
+			}
 		}
 
 		public static void MakeCustomSyncWriter(NetworkIdentity behaviorOwner, Type targetType, Action<NetworkWriter> customSyncObject, Action<NetworkWriter> customSyncVar, NetworkWriter owner, NetworkWriter observer)
