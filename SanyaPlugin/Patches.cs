de1b6faf28460dd5e398eaeda02cea5b900184fe
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2021-01-28 22:58:00 +0900

    remove cuffed-escape counter incrase

--- SanyaPlugin/Patches.cs
+++ SanyaPlugin/Patches.cs
@@ -991,6 +991,31 @@ namespace SanyaPlugin.Patches
 		}
 	}
 
+	//transpiler
+	[HarmonyPatch(typeof(CharacterClassManager), nameof(CharacterClassManager.CallCmdRegisterEscape))]
+	public static class RemoveEscapeCounterPatch
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+			var newInst = instructions.ToList();
+
+			var cuffedClassDindex = newInst.FindIndex(x => x.opcode == OpCodes.Ldsfld 
+				&& x.operand is FieldInfo fieldInfo 
+				&& fieldInfo.Name == nameof(RoundSummary.escaped_scientists)
+			);
+			newInst.RemoveRange(cuffedClassDindex, 4);
+
+			var cuffedScientistindex = newInst.FindLastIndex(x => x.opcode == OpCodes.Ldsfld
+				&& x.operand is FieldInfo fieldInfo
+				&& fieldInfo.Name == nameof(RoundSummary.escaped_ds)
+			);
+			newInst.RemoveRange(cuffedScientistindex, 4);
+
+			for(int i = 0; i < newInst.Count; i++)
+				yield return newInst[i];
+		}
+	}
+
 	// [HarmonyPatch(typeof(NetworkBehaviour), "GetInvokerForHash")]
 	//public static class Patch1
 	//{
