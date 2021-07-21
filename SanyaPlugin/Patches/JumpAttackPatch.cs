3061e92b0e0ab4bcf3089c60c0d3215eacd20fbd
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2021-07-21 15:47:34 +0900

    2.49.1(push kick)

--- SanyaPlugin/Patches/JumpAttackPatch.cs
+++ SanyaPlugin/Patches/JumpAttackPatch.cs
@@ -1,6 +1,7 @@
 ﻿using Exiled.API.Features;
 using HarmonyLib;
 using Mirror;
+using SanyaPlugin.Functions;
 using UnityEngine;
 
 namespace SanyaPlugin.Patches
@@ -15,8 +16,8 @@ namespace SanyaPlugin.Patches
 
 			Log.Debug($"Attack:{__instance.ccm._hub.nicknameSync.MyNick}", SanyaPlugin.Instance.Config.IsDebugged);
 			var player = Player.Get(__instance.gameObject);
-			if(player.SessionVariables.TryGetValue("sanya_kickattack_lasttime", out object time) && (NetworkTime.time - (double)time) < 0.2) return;
-			foreach(var hits in Physics.RaycastAll(__instance.ccm._hub.PlayerCameraReference.position, __instance.ccm._hub.PlayerCameraReference.forward, 1.5f, 4))
+			if(player.SessionVariables.TryGetValue("sanya_kickattack_lasttime", out object time) && (NetworkTime.time - (double)time) < 0.5) return;
+			foreach(var hits in Physics.RaycastAll(__instance.ccm._hub.PlayerCameraReference.position, __instance.ccm._hub.PlayerCameraReference.forward, 2f, 4))
 			{
 				var target = Player.Get(hits.transform.gameObject);
 				if(target?.Id == player?.Id) continue;
@@ -25,7 +26,15 @@ namespace SanyaPlugin.Patches
 				{
 					Log.Debug($"Attack:{player.Nickname} -> {target.Nickname}", SanyaPlugin.Instance.Config.IsDebugged);
 					target.ReferenceHub.falldamage.RpcDoSound();
-					target.Hurt(2f, player, DamageTypes.Scp0492);
+					target.Hurt(3f, player, DamageTypes.Scp0492);
+					player.ShowHitmarker();
+
+					Vector3 vec = __instance.ccm._hub.PlayerCameraReference.forward * 0.5f;
+					Vector3 targetpos = new Vector3(target.Position.x + vec.x, target.Position.y, target.Position.z + vec.z);
+					if(!Methods.IsStuck(targetpos))
+						target.Position = targetpos;
+
+
 					if(!player.SessionVariables.ContainsKey("sanya_kickattack_lasttime"))
 						player.SessionVariables.Add("sanya_kickattack_lasttime", NetworkTime.time);
 					else
