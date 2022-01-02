c9b31c4546691590a589c54598437abf19bef898
初音早猫 <hatsunemiku_39@yahoo.co.jp>
2022-01-02 17:12:02 +0900

    fix index

--- SanyaPlugin/Commands.cs
+++ SanyaPlugin/Commands.cs
@@ -456,7 +456,7 @@ namespace SanyaPlugin.Commands
 								writer.WriteUInt64(1ul);
 								writer.WriteUInt32((uint)1);
 								writer.WriteByte((byte)SyncList<byte>.Operation.OP_SET);
-								writer.WriteUInt32((uint)19);
+								writer.WriteUInt32((uint)18);
 								writer.WriteByte((byte)1);
 							});
 							isActwatchEnabled = true;
@@ -468,7 +468,7 @@ namespace SanyaPlugin.Commands
 								writer.WriteUInt64(1ul);
 								writer.WriteUInt32(1);
 								writer.WriteByte((byte)SyncList<byte>.Operation.OP_SET);
-								writer.WriteUInt32(19);
+								writer.WriteUInt32(18);
 								writer.WriteByte(0);
 							});
 							isActwatchEnabled = false;
